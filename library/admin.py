97712c44d6afcf5b51c91f41c8e74b42a9c82130
Kateryna <rinamajj@gmail.com>
2026-05-21 20:43:12 +0300

    add models Book, Note, Shelf in admin.py

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Book, Note, Shelf
 
-# Register your models here.
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'user', 'status', 'is_favorite', 'created_at', 'updated_at')
+    list_filter = ('status', 'is_favorite', 'genre', 'publisher')
+    search_fields = ('title', 'author', 'publisher')
+    filter_horizontal = ('shelves',)
+
+
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('book', 'user', 'page_number', 'created_at')
+    search_fields = ('book__title', 'content')
+    list_filter = ('created_at',)
+
+
+@admin.register(Shelf)
+class ShelfAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user', 'created_at')
+    search_fields = ('name',)
