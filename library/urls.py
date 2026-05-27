2a2b6c8d5828b8dfc83bb3d719b7e4e55e9bac0c
Kateryna <rinamajj@gmail.com>
2026-05-27 16:26:49 +0300

    add create + read

--- library/urls.py
+++ library/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import test_page
+from .views import register, book_list, book_create
 
 urlpatterns = [
-    path("test/", test_page, name="test_page"),
+    path("auth/register/", register, name="register"),
+    path("", book_list, name="book_list"),
+    path("books/", book_list, name="book_list"),
+    path("books/add/", book_create, name="book_create"),
 ]
--- library/views.py
+++ library/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .forms import BookForm
+from .models import Book
 
 # Create your views here.
 def test_page(request):
     return render(request, "library/test.html")
+
+
+@login_required
+def book_list(request):
+    books = Book.objects.filter(user=request.user)
+    return render(request, "library/book_list.html", {"books": books})
+
+
+@login_required
+def book_create(request):
+    if request.method == "POST":
+        form = BookForm(request.POST)
+        if form.is_valid():
+            book = form.save(commit=False)
+            book.user = request.user
+            book.save()
+            form.save_m2m()
+            return redirect("book_list")
+    else:
+        form = BookForm()
+
+    return render(request, "library/book_form.html", {"form": form})
